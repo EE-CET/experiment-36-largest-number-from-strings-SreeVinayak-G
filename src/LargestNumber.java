064f2446764d6dceea9718b5e4b95aa694ca45c7
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-27 21:45:46 +0530

    Submitted largestNum

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -3,12 +3,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class LargestNumber {
-   
-        // TODO: Read N
-        // TODO: Read N strings into an array
-        // TODO: Sort the strings using a custom comparator
-        // Comparator logic: (str1, str2) -> (str2 + str1).compareTo(str1 + str2)
-        // TODO: Handle edge case where all numbers are "0" (e.g. input 0 0 -> output "0")
-        // TODO: Concatenate sorted strings and print
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                String[] arr = new String[n];
+                for(int i=0;i<n;i++){
+                        arr[i] = sc.next();
+                }
+                Arrays.sort(arr, new Comparator<String>(){
+                        public int compare(String a, String b){
+                                String str1 = a + b;
+                                String str2 = b + a;
+                                return str2.compareTo(str1);
+                        }
+                });
+                System.out.println(String.join("", arr));
+        }
     
 }
